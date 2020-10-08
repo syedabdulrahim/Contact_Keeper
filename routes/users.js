dd06c5e10ff7e4e3504235d2a10d5d96860748e0
Syed abdul rahim <syedabdulrahim4@gmail.com>
2020-10-09 01:22:38 +0530

    adding validaion and routes

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const express=require('express');
+const user=require('../models/Users');
+const { body, validationResult } = require('express-validator');
 
 const router=express.Router();
 
@@ -6,9 +8,28 @@ const router=express.Router();
 //@desc   Register a user
 //@access PUBLIC
 
-router.post('/',(req,resp)=>{
+router.post('/',[
+    body('name','Please add your name')
+    .not()
+    .isEmpty(),
+    body('email','Please add your email-ID').isEmail(),
+    body('password','Please enter a password with 7 or more charecters').isLength({min:7})
+    
 
-    resp.send('Registers a user')
+],(req,res)=>{
+
+    // resp.send('Registers a user');
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        res.status(400).json({error:errors.array()})
+    }
+
+    else{
+        res.status(200).json({status:"OK"})
+    }
+    console.log(req.body);
+    resp.send(req.body);
+
 })
 
 module.exports=router;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express=require('express');
+const connectDB=require('./config/db')
 
 const app=express();
 
 
 const PORT =process.env.PORT||5000;
+
+//body parser
+app.use(express.json({extended:false}))
+//connect db
+connectDB();
+
 
 //DEFINING ROUTES
 
